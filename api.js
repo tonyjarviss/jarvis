2cd291c998dce60ee0070749f2866ebba24b33cf
golivesid <155578467+golivesid@users.noreply.github.com>
2024-02-20 23:11:09 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://afca-174-138-88-233.ngrok-free.app/get?url=${id}`
+            `https://jarvisbot-nine.vercel.app/get?url=${id}`
         );
         return response.data;
     } catch (error) {
