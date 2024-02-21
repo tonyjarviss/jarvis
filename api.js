97ba12793590abc06c6077740fdac675766f1a72
golivesid <155578467+golivesid@users.noreply.github.com>
2024-02-22 00:38:52 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://1897-139-5-249-170.ngrok-free.app/get?url=${id}`
+            `https://df0f-139-5-249-170.ngrok-free.app/get?url=${id}`
         );
         return response.data;
     } catch (error) {
